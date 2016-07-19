a65dd75011d153b856349822ea480700b603c65e
mattlaguardia <matthewlaguardia@gmail.com>
2016-07-19 16:47:43 -0700

    links yelp api and google maps directive

--- client/controllers.js
+++ client/controllers.js
@@ -2,55 +2,61 @@
 //// GOOGLE MAPS
 ////////////////////////
 angular.module("myApp").controller("mapController", function($scope, $http, $interval, uiGmapGoogleMapApi) {
-  var iconimg = {
-        url: 'http://vignette2.wikia.nocookie.net/pokemon/images/1/13/Poke_Ball_Sprite.png/revision/20151205192135'
-    }
-  $scope.loadYelp = function(){
-    $http.get('server/yelpdb')
-      .success(function(response){
-        console.log(response);
-      })
+
+  var pokeBall = {
+    url: 'http://vignette2.wikia.nocookie.net/pokemon/images/1/13/Poke_Ball_Sprite.png/revision/20151205192135'
   }
 
-  angular.extend($scope, {
-    map: {
-      center: {
-        latitude: 37.773972,
-        longitude: -122.431297
-      },
-      zoom: 12,
-      markers: [],
-      events: {
-        click: function (map, eventName, originalEventArgs){
-          var e = originalEventArgs[0];
-          var lat = e.latLng.lat();
-          var lon = e.latLng.lng();
-          var marker = {
-            id: Date.now(),
-            icon: iconimg,
-            coords: {
-              latitude: lat,
-              longitude: lon
-            },
-             options: {
-               labelContent : 'HI BEN',
-               labelAnchor: "16 33",
-               labelClass: 'labelClass',
-               labelStyle: {opacity: 0.75}
-             }
-          };
-          $scope.map.markers.push(marker);
-          console.log($scope.map.markers);
-          $scope.$apply();
+  $http.get('http://localhost:3000/api')
+    .then(function(response){
+      $scope.pokestops = response.data[0];
+      for(var i =0; i < $scope.pokestops.length; i++){
+        var templat = $scope.pokestops[i].location.coordinate.latitude;
+        var templng = $scope.pokestops[i].location.coordinate.longitude;
+        var id = $scope.pokestops[i].id;
+        var marker = {
+          id: id,
+          icon: pokeBall,
+          coords: {latitude: templat, longitude: templng}
         }
+        $scope.map.markers.push(marker)
+      }
+    })
+    // var markers = [{latitude: 37.773972, longitude: -122.431297}]
+  $scope.map={
+    center: {
+      latitude: 37.773972,
+      longitude: -122.431297
+    },
+    zoom: 12,
+    bounds: {},
+    markers: [],
+    events: {
+      click: function (map, eventName, originalEventArgs){
+        var e = originalEventArgs[0];
+        var lat = e.latLng.lat();
+        var lon = e.latLng.lng();
+        var marker = {
+          id: Date.now(),
+          icon: pokeBall,
+          coords: {
+            latitude: lat,
+            longitude: lon
+          },
+           options: {
+             labelContent : 'HI BEN',
+             labelAnchor: "16 33",
+             labelClass: 'labelClass',
+             labelStyle: {opacity: 0.75}
+           }
+        };
+        $scope.map.markers.push(marker);
+        console.log(marker);
+        $scope.$apply();
       }
     }
-  })
+  }
 });
-//////////////////////
-///// YELP
-/////////////////////
-
 
 angular.module('myApp').controller('loginController',
   ['$scope', '$location', 'AuthService',
